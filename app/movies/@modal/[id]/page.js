93e8f6eb45272a3399831c0ae74bf219e8c5cdb2
michaelwitk <mwit2023@gmail.com>
2023-11-17 18:51:08 +0100

    fix

--- app/movies/@modal/[id]/page.js
+++ app/movies/@modal/[id]/page.js
@@ -1,31 +1,11 @@
-'use client'
-
-import { useRouter } from 'next/navigation'
-
-const LinkBack = ({ href }) => {
-  const router = useRouter()
-  return (
-    <a
-      className="active:scale-50 transition"
-      href="/movies"
-      onClick={(event) => {
-        event.preventDefault()
-        // router.back() // works but not ideal
-        router.push('/movies') // <Link> issue, renders
-      }}
-    >
-      close modal
-    </a>
-  )
-}
+import Link from 'next/link'
 
 const Page = ({ params: { id } }) => {
-  console.log('render app/movies/@modal/[id]/page', { id })
   return (
     <div className="fixed top-0 left-0 h-screen w-screen bg-black/80">
       <div className="bg-white p-2">
         <div className="flex justify-end">
-          <LinkBack href="/movies">close modal</LinkBack>
+          <Link href="/movies">close modal</Link>
         </div>
 
         <div>modal id={id}</div>
